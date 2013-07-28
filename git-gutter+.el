7438c8fb9ae4597d1fcc99845472640013984259
vmfhrmfoaj <vmfhrmfoaj@yahoo.com>
2013-07-28 15:20:26 +0900

    Fix for fail `git-gutter+-next-hunk' function, If read-only-mode on the diff-mode.
    I have not change the patch file in general.
    So I prevent to writing on the diff-mode.
    Therefore, fail when try to change `*git-gutter+-diff*' buffer in `git-gutter+-popup-hunk'

--- git-gutter+.el
+++ git-gutter+.el
@@ -489,9 +489,11 @@ calculated width looks wrong. (This can happen with some special characters.)"
                         (git-gutter+-search-here-diffinfo git-gutter+-diffinfos))
     (save-selected-window
       (with-current-buffer (get-buffer-create git-gutter+-popup-buffer)
+        (read-only-mode -1)
         (erase-buffer)
         (insert (plist-get it :content))
         (insert "\n")
+        (read-only-mode 1)
         (goto-char (point-min))
         (diff-mode)
         (pop-to-buffer (current-buffer))))))
