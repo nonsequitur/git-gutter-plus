25f29d334fd48f960b9148243d4d11276ac26ad0
Cornelius Mika <cornelius.mika@gmail.com>
2014-02-05 16:59:52 +0100

    Fix required version of `git-commit-mode'

--- git-gutter+.el
+++ git-gutter+.el
@@ -19,7 +19,7 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;; Package-Requires: ((git-commit-mode "1.0"))
+;; Package-Requires: ((git-commit-mode "0.14"))
 
 ;;; Commentary:
 ;;
