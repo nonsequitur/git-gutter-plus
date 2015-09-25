93aa5ed676ccd278f0f10de4ca0b7de27abc6a8a
Cornelius Mika <cornelius.mika@gmail.com>
2015-09-25 11:05:28 +0200

    Add 'dash' as a package dependency

--- git-gutter+.el
+++ git-gutter+.el
@@ -19,7 +19,7 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;; Package-Requires: ((git-commit "0"))
+;; Package-Requires: ((git-commit "0") (dash "0"))
 ;; Keywords: git vc
 
 ;;; Commentary:
